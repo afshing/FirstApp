7c4519b49d0d60e9f14749e73376c10c55712971
Afshin Moazami <moazzami.afshin@gmail.com>
2014-08-25 13:26:42 -0400

    First Rails Commit

---
+++ config/environments/development.rb
@@ -0,0 +1,40 @@
+require 'tzinfo'
+FirstApp::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+  
+  config.secret_key_base = '5491c7127f542736def4f0c1b53ca0cd27acc5f8e28f0c2a3835aa9aba1cdd1d44d6d4001f909e9d085cfc8f8af22c9d1f6eff188c34dd22e112f6de1a040813'
+end
